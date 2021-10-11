dc676164a8fb89976583ee7a148e61e49193a8cf
Michael-Bryant Choa <mbchoa@gmail.com>
2021-10-11 20:02:10 +0800

    Added page layout templates and basic styling

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+const CleanCss = require('clean-css');
+
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addFilter('cssmin', function(code) {
+    return new CleanCss({}).minify(code).styles;
+  });
+};
