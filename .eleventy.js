e567d79716fbc3c6be7ecd38f3aca4c681d3b74a
Michael-Bryant Choa <mbchoa@gmail.com>
2021-10-17 22:55:50 +0800

    feat: added test post
    * Reorganized _includes directory with /components and /layouts sub-directories
    * Added new post page layout for individual post
    * Added new custom Eleventy filters to format each post's title and their permalinks based on post date

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,19 @@
 const CleanCss = require('clean-css');
+const format = require('date-fns/format');
 
 module.exports = function (eleventyConfig) {
+	// Minifies CSS
 	eleventyConfig.addFilter('cssmin', function(code) {
     return new CleanCss({}).minify(code).styles;
+  });
+
+	// Date formatting (human readable)
+	eleventyConfig.addFilter('postTitle', (dateISOString) => {
+    return format(new Date(dateISOString), "MMMM do 'at' hh:mmaaa ");
+  });
+
+  // Date formatting (machine readable)
+  eleventyConfig.addFilter('postUrlSlug', (dateISOString) => {
+		return new Date(dateISOString).getTime();
   });
 };
