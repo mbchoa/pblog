a4fb1a58a8e3cb5a42dba7094f14b110131bd9e0
Michael-Bryant Choa <mbchoa@gmail.com>
2021-11-15 12:39:37 -0500

    feat: add "NYC Winter" post

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
+const axios = require('axios');
 const CleanCss = require('clean-css');
-const { format, utcToZonedTime } = require('date-fns-tz');
+const heicConvert = require('heic-convert');
+const format = require('date-fns/format');
 
 module.exports = function (eleventyConfig) {
 	// Minifies CSS
@@ -9,12 +11,28 @@ module.exports = function (eleventyConfig) {
 
 	// Date formatting (human readable)
 	eleventyConfig.addFilter('postTitle', (dateISOString) => {
-    return format(utcToZonedTime(new Date(dateISOString), 'America/Los_Angeles'), "MMMM do 'at' h:mmaaa");
+    return format(new Date(dateISOString), "MMMM do 'at' h:mmaa")
   });
 
   // Date formatting (machine readable)
   eleventyConfig.addFilter('postUrlSlug', (dateISOString) => {
 		return new Date(dateISOString).getTime();
+  });
+
+  eleventyConfig.addShortcode('heicImage', async (url, ...imgAttr) => {
+    const outputBuffer = await axios.get(url, { responseType: 'arraybuffer' })
+      .then(async (response) => {
+        return await heicConvert({
+          buffer: response.data,
+          format: 'JPEG',
+          quality: 1
+        });
+      });
+    
+    const jpegBase64 = outputBuffer.toString('base64');
+    const attributesString = imgAttr.reduce((output, attribute) => `${output} ${attribute}`, '');
+    console.log(attributesString);
+    return `<img src="data:image/jpeg;base64, ${jpegBase64}" ${attributesString}>`;
   });
 
   // Instructs Eleventy compilation to ignore processing the following
---
+++ posts/nyc-winter.md
@@ -0,0 +1,8 @@
+---
+title: NYC Winter
+description: A visit to winter NYC
+date: 2021-11-15T16:57:44.333
+---
+<p>Hello again New York City 🍎</p>
+
+{% heicImage "https://www.michaelchoa.com/images/uploads/img_7565.heic", 'alt="A look outside an NYC apartment."', "width=556", "height=742" %}
